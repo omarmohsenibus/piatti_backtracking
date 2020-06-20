5862b12f25ed8405f092998559e006208b960ed3
Omar Mohseni <omarmohseni.bus@gmail.com>
2020-06-20 11:27:56 +0200

    add tipologie label

--- main.c
+++ main.c
@@ -1,19 +1,24 @@
 #include "piatti.h"
 int main(void) {
 	struct piatto piatti[] = {
-		{PRIMO,     100, "Gnocchi di patate"},
-		{PRIMO,     200, "Lasagne alla bolognese"},
-		{CONTORNO,  300, "Sformato di verdure"},
-		{CONTORNO,  300, "Broccoli gratinati"},
-		{SECONDO,   300, "Parmigiana di melanzane"},
 		{SECONDO,   200, "Pollo alle mandorle"},
+		{PRIMO,     100, "Gnocchi di patate"},
+		{CONTORNO,  300, "Broccoli gratinati"},
+		{PRIMO,     200, "Lasagne alla bolognese"},
+		{SECONDO,   300, "Parmigiana di melanzane"},
+		{CONTORNO,  300, "Sformato di verdure"},
 	};
 	size_t n = 6;
+	char *tipologie[] = {
+		"PRIMO",
+		"SECONDO",
+		"CONTORNO"
+	};
 
 	printf("Piatti che Maria puo cucinare per la sua famiglia:\n\n");
-	for (size_t i = 0; i < n; i++) {
-		printf("{ Nome: %s\tTipologia: %d\tKcal: %d }\n", piatti[i].nome, piatti[i].tipologia, piatti[i].Kcal);
-	}
+	for (size_t i = 0; i < n; i++) 
+		printf("{ Nome: %s\tTipologia: %s\tKcal: %d }\n", piatti[i].nome, tipologie[piatti[i].tipologia - 1], piatti[i].Kcal);
+
 
 	printf("\n");
 
